324c99717e1578dab57a812c6db53b5820c6bb32
FnnDev <fnn00900@gmail.com>
2025-12-15 10:53:49 +0330

    start project

---
+++ app/src/main/java/com/fnndev/debtbook/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.fnndev.debtbook
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.fnndev.debtbook.ui.theme.DebtBookTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DebtBookTheme {
+
+            }
+        }
+    }
+}
